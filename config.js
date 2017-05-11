ac6627cf6022f5c12f64417851f879e93608b971
mj-e <matt_evans30@yahoo.com>
2017-05-11 14:44:03 +0100

    added basic server and routes, config setup connected to local mongodb

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    DB: {
+        test: 'mongodb://localhost/northcoders-news-api-test',
+        dev: 'mongodb://localhost/northcoders-news-api-live'
+    },
+    PORT: {
+        test: 3090, 
+        dev: 3000
+    }
+};
---
+++ server.js
@@ -0,0 +1,32 @@
+if (!process.env.NODE_ENV) process.env.NODE_ENV = 'dev';
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const config = require('./config');
+const apiRouter = require('./routes/routes');
+const cors = require('cors');
+const DB = config.DB[process.env.NODE_ENV] || process.env.DB;;
+const PORT = config.PORT[process.env.NODE_ENV] || process.env.PORT;
+const app = express();
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect(DB, function (err){
+    if(!err){
+        console.log(`connected to the Database: ${DB}`);
+    } else {
+        console.log(`error connecting to the Database ${err}`);
+    }
+});
+
+app.use('/api', apiRouter); 
+
+app.use('/*', function (request, response) {
+  response.status(404).send({reason: 'ROUTE NOT FOUND'})
+})
+
+app.listen(PORT, function () {
+  console.log(`listening on port ${PORT}`);
+});

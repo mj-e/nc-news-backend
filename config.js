799e7a1c09a598ec05948e1e79dcf19411cf658d
mj-e <matt_evans30@yahoo.com>
2017-05-15 13:17:33 +0100

    changed mlab DB

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 module.exports = {
     DB: {
         test: 'mongodb://localhost/northcoders-news-api-test',
-        dev: 'mongodb://nc-news:password@ds137121.mlab.com:37121/nc-news-backend'
+        dev: 'mongodb://nc-news:password@ds143241.mlab.com:43241/nc-news-backend'
     },
     PORT: {
         test: 3090, 
         dev: 3000
     }
 };
-// mongodb://<dbuser>:<dbpassword>@ds137121.mlab.com:37121/nc-news-backend
